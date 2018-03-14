1017eca180ccd8ce9692e67d97f00aaab8992993
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-03-14 15:37:43 -0400

    Add topic and topic post routes to route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
-  
+  resources :posts, only: [:index, :create, :new]
+
+  resources :topics, only: [:index] do
+    resources :posts
+  end
+
   root 'posts#index'
 end
